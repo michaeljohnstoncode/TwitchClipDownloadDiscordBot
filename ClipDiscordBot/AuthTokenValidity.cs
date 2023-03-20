71e163c94d9df5b9da2cc0525767b72ca3c5d0b9
Michael Johnston <michael.johnston6042@gmail.com>
2023-03-20 13:02:24 -0400

    added case fix where twitch auth code fails. and fix for ytdlp error

--- ClipDiscordBot/AuthTokenValidity.cs
+++ ClipDiscordBot/AuthTokenValidity.cs
@@ -11,6 +11,7 @@ namespace ClipDiscordBot
         private readonly HttpClient _httpClient;
         private DeserializeConfigJson _configJson;
         private Discord.Commands.SocketCommandContext _discordContext;
+        private bool isAuthCodeInvalid;
         public AuthTokenValidity(HttpClient httpClient, DeserializeConfigJson configJson)
         {
             _httpClient = httpClient;
@@ -38,6 +39,12 @@ namespace ClipDiscordBot
 
             if (string.IsNullOrEmpty(twitchAuthCode))
                 await NewAuthCode(_discordContext);
+
+            if(isAuthCodeInvalid == true)
+            {
+                await NewAuthCode(_discordContext);
+                isAuthCodeInvalid = false;
+            }
 
             if (string.IsNullOrEmpty(twitchAuthToken))
                 await NewAuthToken();
@@ -87,6 +94,13 @@ namespace ClipDiscordBot
 
                 response = await _httpClient.SendAsync(request);
                 authTokenJson = await response.Content.ReadAsStringAsync();
+            }
+
+            //in the case twitch auth code fails (unknown why), try to get a new auth code
+            if (authTokenJson.Contains("Invalid authorization code"))
+            {
+                ValidateTokens();
+                isAuthCodeInvalid = true;
             }
 
             var authTokenInfo = JsonConvert.DeserializeObject<TwitchAuthToken>(authTokenJson);
